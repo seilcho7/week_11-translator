ae4205b5de6f75bcbdf4c7a8900886ade583b719
Seil Cho <seilcho7@hotmail.com>
2019-04-24 11:02:43 -0400

    adds .map rendering

--- src/App.js
+++ src/App.js
@@ -15,28 +15,41 @@ class App extends React.Component {
   }
 
   render() {
+    console.log('Rendering App');
+    console.log(`Top of App.render, State is now ${this.state.inputText}`);
+    const names = ["oakley", "milla", "angela"];
+
     return (
       <div className="App">
         <header className="App-header">
-          <input
-            onChange={ (e) => {
-              console.log("onChange event fired");
-              console.log(e.target.value);
+          <ul>
+            {
+              names.map(n => <li>{n}</li>)
+            }
+          </ul>
 
-              // This is a request for a change that will take place in the future
-              this.setState({
-                inputText: e.target.value
-              }, () => {
-                console.log(`After setState, State is not ${this.state.inputText}`)
-            });
-          } }
-          />
-          <Output
-            text={this.state.inputText}
-          />
+          {/* <input
+            onChange={ this._updateInputText }
+          /> */}
+          {/* <Output
+            text={ this.state.inputText }
+          /> */}
+
         </header>
       </div>
     );
+  }
+
+  _updateInputText = (e) => {
+    console.log("onChange event fired");
+    console.log(e.target.value);
+
+    // This is a request for a change that will take place in the future
+    this.setState({
+      inputText: e.target.value
+    }, () => {
+      console.log(`After setState, State is not ${this.state.inputText}`);
+    });
   }
 }
 
